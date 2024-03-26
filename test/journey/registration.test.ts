665156090e63523de1bc2a8796eaf29fe425f611
Wayne Starr <me@racer159.com>
2024-03-26 14:56:28 -0600

    wip: move some things around

---
+++ test/journey/registration.test.ts
@@ -0,0 +1,7 @@
+import { expect, test} from '@jest/globals';
+import { K8s, kind } from "kubernetes-fluent-client";
+import { spawnSync } from "child_process";
+
+test('test kicking off a pipeline run', async () => {
+
+}, 30000);
