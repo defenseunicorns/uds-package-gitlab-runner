ef5f7161a8f35e673168cd0b264d560b5f2fca5a
Wayne Starr <me@racer159.com>
2024-03-26 15:39:45 -0600

    swap over to jest tests

--- test/journey/registration.test.ts
+++ test/journey/registration.test.ts
@@ -2,6 +2,21 @@ import { expect, test} from '@jest/globals';
 import { K8s, kind } from "kubernetes-fluent-client";
 import { spawnSync } from "child_process";
 
-test('test kicking off a pipeline run', async () => {
-
+test('test registration of the runner succeeded', async () => {
+    let foundRegistrationSuccess = false
+    for (let i = 0; i < 3; i++) {
+        await new Promise(r => setTimeout(r, 3000))
+        const runnerPods = await K8s(kind.Pod).InNamespace("gitlab-runner").WithLabel("app", "gitlab-runner").Get()
+        if (runnerPods.items.at(0)?.status?.phase === "Running") {
+            const podName = runnerPods.items.at(0)?.metadata?.name
+            const runnerLogs = spawnSync(
+                `uds zarf tools kubectl logs -n gitlab-runner ${podName} -c gitlab-runner`, {
+                shell: true, // Run command in a shell
+            });
+            if (runnerLogs.stdout.indexOf("Registering runner... succeeded") > -1) {
+                foundRegistrationSuccess = true
+            }
+        }
+    }
+    expect(foundRegistrationSuccess).toBe(true)
 }, 30000);
