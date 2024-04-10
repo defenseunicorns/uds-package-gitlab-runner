6c60a90feaefc7873b2bd87ace27e1beb198845d
Wayne Starr <Racer159@users.noreply.github.com>
2024-04-10 16:54:38 -0600

    chore: improve jest test patterns (#73)
    ## Description
    
    This improves the patterns for running jest tests for GLR - we decided
    against cypress and playwright because both run the tests in browser and
    cannot make use of spawnSync or KFC.
    
    ## Related Issue
    
    Fixes #72 
    
    ## Type of change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [X] Other (security config, docs update, etc)
    
    ## Checklist before merging
    
    - [X] Test, docs, adr added or updated as needed
    - [X] [Contributor Guide
    Steps](https://github.com/defenseunicorns/uds-package-gitlab-runner/blob/main/CONTRIBUTING.md#developer-workflow)
    followed
    
    Co-authored-by: Billy Figueroa <figuer25@gmail.com>

--- test/journey/registration.test.ts
+++ test/journey/registration.test.ts
@@ -1,22 +1,18 @@
 import { expect, test} from '@jest/globals';
 import { K8s, kind } from "kubernetes-fluent-client";
-import { spawnSync } from "child_process";
+import { zarfExec, retry } from "../common";
 
 test('test registration of the runner succeeded', async () => {
-    let foundRegistrationSuccess = false
-    for (let i = 0; i < 3; i++) {
-        await new Promise(r => setTimeout(r, 3000))
+    let foundRegistrationSuccess = await retry(async () => {
         const runnerPods = await K8s(kind.Pod).InNamespace("gitlab-runner").WithLabel("app", "gitlab-runner").Get()
         if (runnerPods.items.at(0)?.status?.phase === "Running") {
-            const podName = runnerPods.items.at(0)?.metadata?.name
-            const runnerLogs = spawnSync(
-                `uds zarf tools kubectl logs -n gitlab-runner ${podName} -c gitlab-runner`, {
-                shell: true, // Run command in a shell
-            });
+            const podName = runnerPods.items.at(0)?.metadata?.name!
+            const runnerLogs = zarfExec(["tools", "kubectl", "logs", "-n", "gitlab-runner", podName, "-c", "gitlab-runner"], true);
             if (runnerLogs.stdout.indexOf("Registering runner... succeeded") > -1) {
-                foundRegistrationSuccess = true
+                return true
             }
         }
-    }
+        return false
+    })
     expect(foundRegistrationSuccess).toBe(true)
 });
