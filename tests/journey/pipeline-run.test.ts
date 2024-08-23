5d84cea91e8e7bd3522fab8e0cc83efdec7a45ab
Eric Wyles <23637493+ericwyles@users.noreply.github.com>
2024-08-23 15:45:23 -0500

    prying the semicolons from my cold, dead hands

--- tests/journey/pipeline-run.test.ts
+++ tests/journey/pipeline-run.test.ts
@@ -1,36 +1,36 @@
-import { expect, test } from '@jest/globals';
+import { expect, test} from '@jest/globals';
+import { K8s, kind } from "kubernetes-fluent-client";
 import { zarfExec, retry } from "../common";
 import * as path from 'path';
 import { execSync } from 'child_process';
-import { K8s, kind } from "kubernetes-fluent-client";
 import { rm } from 'fs/promises';
 
 test('hello kitteh succeeds', async () => {
     const sourceRepoName = 'kitteh'
-    const expectedStatus = "success"
-    const expectedJobLogOutputs: string[] = ['Hello Kitteh']; 
+    const expectedStatus = 'success'
+    const expectedJobLogOutputs: string[] = ['Hello Kitteh']
 
-    await executeTest(sourceRepoName, expectedJobLogOutputs, expectedStatus);
+    await executeTest(sourceRepoName, expectedJobLogOutputs, expectedStatus)
 
 }, 90000);
 
 
 test('podman succeeds', async () => {
     const sourceRepoName = 'podman'
-    const expectedStatus = "success"
-    const expectedJobLogOutputs: string[] = ['STEP 1/2: FROM scratch', 'STEP 2/2: ADD test.txt /', 'COMMIT']; 
+    const expectedStatus = 'success'
+    const expectedJobLogOutputs: string[] = ['STEP 1/2: FROM scratch', 'STEP 2/2: ADD test.txt /', 'COMMIT']
 
-    await executeTest(sourceRepoName, expectedJobLogOutputs, expectedStatus);
+    await executeTest(sourceRepoName, expectedJobLogOutputs, expectedStatus)
 }, 90000);
 
 
 test('podman fails', async () => {
 
     const sourceRepoName = 'podman'
-    const expectedStatus = "failed"
-    const expectedJobLogOutputs: string[] = []; 
+    const expectedStatus = 'failed'
+    const expectedJobLogOutputs: string[] = []
 
-    await executeTest(sourceRepoName, expectedJobLogOutputs, expectedStatus);
+    await executeTest(sourceRepoName, expectedJobLogOutputs, expectedStatus)
 }, 90000);
 
 
@@ -38,19 +38,19 @@ async function executeTest(sourceRepoName: string, expectedJobLogOutputs: string
     const nowMillis = Date.now();
     const tokenName = `if-you-see-me-in-production-something-is-horribly-wrong-${nowMillis}`    
     
-    var sourceDir = path.join(__dirname, 'repo-sources', sourceRepoName);
+    var sourceDir = path.join(__dirname, 'repo-sources', sourceRepoName)
 
     // Get the toolbox pod and add a token to the root GitLab user
     await createToken(tokenName);
-    const headers: HeadersInit = [["PRIVATE-TOKEN", tokenName]];
+    const headers: HeadersInit = [["PRIVATE-TOKEN", tokenName]]
 
-    const gitLabProjectName = `${sourceRepoName}-${nowMillis}`;
-    const projectId = await createNewGitlabProject(sourceDir, tokenName, gitLabProjectName, headers);
+    const gitLabProjectName = `${sourceRepoName}-${nowMillis}`
+    const projectId = await createNewGitlabProject(sourceDir, tokenName, gitLabProjectName, headers)
     
-    await unprotectRunner(headers, tokenName);
+    await unprotectRunner(headers, tokenName)
 
-    // Check that the pipeline actually ran as expected
-    await checkJobResults(projectId, headers, expectedJobLogOutputs, expectedStatus);
+    // Check that the pipeline ran as expected
+    await checkJobResults(projectId, headers, expectedJobLogOutputs, expectedStatus)
 }
 
 
@@ -58,7 +58,6 @@ async function createToken(tokenName: string) {
     var nowMillis = Date.now();
     const toolboxPods = await K8s(kind.Pod).InNamespace("gitlab").WithLabel("app", "toolbox").Get();
     const toolboxPod = toolboxPods.items.at(0);
-    console.log('Using gitlab-rails runner to configure root token');
     zarfExec(["tools",
         "kubectl",
         "--namespace", "gitlab",
@@ -73,29 +72,29 @@ async function createToken(tokenName: string) {
 async function createNewGitlabProject(sourceDir: string, tokenName: string, gitLabProjectName: string, headers: HeadersInit) {
     //console.log(`Setting up new git repo at ${gitRepoDir}`);
     //copyFilesToGitRepoDir(sourceDir, gitRepoDir);
-    const millis = Date.now();
+    const millis = Date.now()
     await deleteDirectory(path.join(sourceDir, '.git')) 
-    execSync('git init', { cwd: sourceDir });
-    execSync('git add . ', { cwd: sourceDir });
-    execSync('git config commit.gpgsign false', { cwd: sourceDir }); // need this so that gpg signing doesn't attempt to happen locally when running tests
-    execSync('git commit -m "Initial commit" ', { cwd: sourceDir });
-    execSync(`git remote add origin https://root:${tokenName}@gitlab.uds.dev/root/${gitLabProjectName}.git`, { cwd: sourceDir });
-    execSync('git push -u origin --all', { cwd: sourceDir });
+    execSync('git init', { cwd: sourceDir })
+    execSync('git add . ', { cwd: sourceDir })
+    execSync('git config commit.gpgsign false', { cwd: sourceDir }) // need this so that gpg signing doesn't attempt to happen locally when running tests
+    execSync('git commit -m "Initial commit" ', { cwd: sourceDir })
+    execSync(`git remote add origin https://root:${tokenName}@gitlab.uds.dev/root/${gitLabProjectName}.git`, { cwd: sourceDir })
+    execSync('git push -u origin --all', { cwd: sourceDir })
     await deleteDirectory(path.join(sourceDir, '.git'))
 
     console.log(`Finding project id for project name [${encodeURIComponent(gitLabProjectName)}]`)
-    const projectResp = await fetch(`https://gitlab.uds.dev/api/v4/projects?search=${encodeURIComponent(gitLabProjectName)}`, { headers });
-    const projects = await projectResp.json();
+    const projectResp = await fetch(`https://gitlab.uds.dev/api/v4/projects?search=${encodeURIComponent(gitLabProjectName)}`, { headers })
+    const projects = await projectResp.json()
 
-    const project = projects.find((p: { name: string; }) => p.name === gitLabProjectName);
-    const projectId = project?.id;
+    const project = projects.find((p: { name: string; }) => p.name === gitLabProjectName)
+    const projectId = project?.id
     console.log(`Found project id [${projectId}]`)
-    return projectId;
+    return projectId
 }
 
 async function unprotectRunner(headers: HeadersInit, tokenName: string) {
-    const runnerIDResp = await (await fetch(`https://gitlab.uds.dev/api/v4/runners/all`, { headers })).json();
-    const runnerID = runnerIDResp[0].id;
+    const runnerIDResp = await (await fetch(`https://gitlab.uds.dev/api/v4/runners/all`, { headers })).json()
+    const runnerID = runnerIDResp[0].id
     const runnerResp = await fetch(`https://gitlab.uds.dev/api/v4/runners/${runnerID}`, {
         headers: [
             ["PRIVATE-TOKEN", tokenName],
@@ -104,38 +103,38 @@ async function unprotectRunner(headers: HeadersInit, tokenName: string) {
         body: "access_level=not_protected",
         method: "put"
     });
-    expect(runnerResp.status).toBe(200);
+    expect(runnerResp.status).toBe(200)
 }
 
 async function checkJobResults(projectId: any, headers: HeadersInit, expectedJobLogOutputs: string[], expectedStatus: string) {
     let status = await retry(async () => {
-        const jobIDResp = await (await fetch(`https://gitlab.uds.dev/api/v4/projects/${projectId}/jobs`, { headers })).json();
+        const jobIDResp = await (await fetch(`https://gitlab.uds.dev/api/v4/projects/${projectId}/jobs`, { headers })).json()
 
         // Print the job response (useful for debugging)
-        console.log(jobIDResp);
+        console.log(jobIDResp)
 
         if (jobIDResp.length > 0 && (jobIDResp[0].status === "success" || jobIDResp[0].status === "failed")) {
             const jobID = jobIDResp[0].id;
-            const jobLog = await (await fetch(`https://gitlab.uds.dev/api/v4/projects/${projectId}/jobs/${jobID}/trace`, { headers })).text();
+            const jobLog = await (await fetch(`https://gitlab.uds.dev/api/v4/projects/${projectId}/jobs/${jobID}/trace`, { headers })).text()
 
             // Print the job log (useful for debugging)
             console.log(jobLog);
 
             expectedJobLogOutputs.forEach( expectedOutput => {
-                expect(jobLog).toContain(expectedOutput);
+                expect(jobLog).toContain(expectedOutput)
             });
             return jobIDResp[0].status;
         }
-        return false;
+        return false
     }, 7, 7000);
-    expect(status).toBe(expectedStatus);
+    expect(status).toBe(expectedStatus)
 }
 
 async function deleteDirectory(path: string) {
     try {
-        await rm(path, { recursive: true, force: true });
-        console.log(`Directory ${path} has been deleted successfully.`);
+        await rm(path, { recursive: true, force: true })
+        console.log(`Directory ${path} has been deleted successfully.`)
     } catch (error) {
-        console.error(`Error while deleting directory ${path}:`, error);
+        console.error(`Error while deleting directory ${path}:`, error)
     }
 }
