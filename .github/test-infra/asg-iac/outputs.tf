2db1636173737215d5e67452748814cce2d5084b
Wayne Starr <me@racer159.com>
2024-09-05 08:08:55 -0600

    test with full arn

--- .github/test-infra/asg-iac/outputs.tf
+++ .github/test-infra/asg-iac/outputs.tf
@@ -1,3 +1,8 @@
+output "asg_arn" {
+  value = aws_autoscaling_group.uds-package-gitlab-runner.arn
+  description = "The ARN of the Autoscaling Group"
+}
+
 output "asg_role_arn" {
   value = aws_iam_role.asg_role.arn
   description = "The ARN of the ASG IRSA role"
